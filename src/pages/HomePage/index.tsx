17c71b17d0f4cdfdd712e4cbed73c97e211ca928
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-02 17:25:20 -0300

    fixing local Introduction

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,16 @@
-import { Indtroduction } from "../../components/Introduction";
+import { AboutMe } from "../../components/AboutMe";
+import { Indtroduction } from "../../components/Logo/Introduction";
 import { Projects } from "../../components/Projects";
 
 export const HomePage = () => {
 
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen">
+    <div className="flex flex-col items-center justify-start min-h-screen gap-12">
       <Indtroduction />
       <div className="flex flex-col w-full px-5">
         <Projects />
       </div>
+      <AboutMe />
     </div>
   );
 
