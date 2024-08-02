4992c6930b277c68fa0e9d6f280f499a8d30c5a1
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-02 17:25:33 -0300

    feat: new components ContactSocialMedia and Footer

---
+++ src/components/ContactSocialMedia/index.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import {
+  FaLinkedinIn,
+  FaBehance,
+  FaWhatsapp,
+  FaInstagram,
+} from "react-icons/fa";
+
+type ContactSocialMediaProps = {
+  size: "small" | "normal" | "large"
+}
+
+export const ContactSocialMedia = ({ size }:ContactSocialMediaProps) => {
+
+  const [iconProps, setIconProps] = useState({
+    size: 40
+  })
+
+  useEffect(() => {
+    switch (size) {
+      case "small":
+        setIconProps({ size: 20 })
+        break;
+      case "normal":
+        setIconProps({ size: 40 })
+        break;
+      case "large":
+        setIconProps({ size: 60 })
+        break;
+      default:
+        break;
+    }
+  }, [setIconProps, size])
+
+  return (
+    <div className="flex items-center justify-center gap-6 text-colorContrast">
+      <a href="https://www.linkedin.com/in/jhonathanoliveira3d/" target="_blank">
+        <FaLinkedinIn {...iconProps}/>
+      </a>
+      <a href="https://www.behance.net/jhonathanoliveira" target="_blank">
+        <FaBehance {...iconProps}/>
+      </a>
+      <a href="https://wa.me/556798891617" target="_blank">
+        <FaWhatsapp {...iconProps}/>
+      </a>
+      <a href="https://www.instagram.com/jhonmotion3d_/" target="_blank">
+        <FaInstagram {...iconProps}/>
+      </a>
+    </div>
+  );
+};
