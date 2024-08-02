92971d3ac189398f91071b4f40f14417bf183dae
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-01 22:18:54 -0300

    feat: animation on menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { Logo } from "../Logo";
 import { Menu } from "../Menu";
 import { CiMenuFries } from "react-icons/ci";
+import { IoCloseOutline } from "react-icons/io5";
 import useThemeContext from "../../hooks/useThemeContext";
+import { AnimatePresence } from "framer-motion";
 
 export const Header = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
@@ -17,10 +19,14 @@ export const Header = () => {
     <header className="fixed w-full bg-zinc-800 rounded-lg flex items-center justify-between px-2 h-36">
       <button onClick={toggleTheme} className="bg-colorContrast">TEMA</button>
       <Logo />
-      <CiMenuFries size={45} onClick={toggleMenuIsOpen}/>
       {
-        menuIsOpen && <Menu />
+        menuIsOpen ? <IoCloseOutline size={45} onClick={toggleMenuIsOpen}/> : <CiMenuFries size={45} onClick={toggleMenuIsOpen}/>
       }
+      <AnimatePresence>
+        {
+          menuIsOpen && <Menu />
+        }
+      </AnimatePresence>
     </header>
   )
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,54 @@
+import { motion } from "framer-motion";
+
 export const Menu = () => {
+  const menuVariants = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.3,
+        ease: [0.12, 0, 0.39, 0],
+      },
+    },
+    exit: {
+      scaleY: 0,
+      transition: {
+        duration: 0.5,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+  };
+
+  const itensVariants = {
+    initial: {
+      y: "5vh",
+      transition: {
+        duration: 0.4
+      }
+    },
+    open: {
+      y: 0,
+      transition: {
+        duration: 0.6
+      }
+    }
+  };
+
   return (
-    <aside className="absolute right-0 top-36 bg-colorPrimary w-full">
-      <nav className="h-full">
+    <motion.aside
+      variants={menuVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="absolute right-0 top-36 bg-colorPrimary w-full origin-top-right"
+    >
+      <motion.nav
+        variants={itensVariants}
+        initial="initial"
+        animate="open"
+      >
         <ul className="flex flex-col items-center justify-start h-full gap-16 my-20">
           <li className="text-4xl">Home</li>
           <li className="text-4xl">About Me</li>
@@ -9,7 +56,7 @@ export const Menu = () => {
           <li className="text-4xl">Projects</li>
           <li className="text-4xl">Contact</li>
         </ul>
-      </nav>
-    </aside>
-  )
-}
+      </motion.nav>
+    </motion.aside>
+  );
+};
