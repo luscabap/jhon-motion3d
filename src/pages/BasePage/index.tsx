f1cf51ac68ea46667132bc49ebdfee5a16ea8542
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-03 13:20:48 -0300

    feat: parallax section effect

--- src/pages/BasePage/index.tsx
+++ src/pages/BasePage/index.tsx
@@ -1,34 +1,45 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { TiMessageTyping } from "react-icons/ti";
 import { useState } from "react";
 import { ModalContact } from "../../components/ModalContact";
 import { IoCloseOutline } from "react-icons/io5";
+import { Indtroduction } from "../../components/Logo/Introduction";
 
 export const BasePage = () => {
   const [modalContactIsOpen, setModalContactIsOpen] = useState(false);
 
   const toggleModalContactIsOpen = () => {
-    setModalContactIsOpen(pv => !pv)
-  }
+    setModalContactIsOpen((pv) => !pv);
+  };
 
   return (
-    <main 
-      className="font-fontMain min-h-screen bg-gradient-background transition-all duration-500 ease-in-out text-colorTextPrimary text-2xl"
-    >
+    <main className="relative font-fontMain min-h-screen bg-gradient-background transition-all duration-500 ease-in-out text-colorTextPrimary text-2xl ">
       <Header />
-      <div className="bg-colorContrast right-2 bottom-2 rounded-full flex items-center justify-center p-1 fixed z-50">
-        { modalContactIsOpen ? <IoCloseOutline size={50} className="w-16 h-16 text-colorTextPrimary" onClick={toggleModalContactIsOpen}/> : <TiMessageTyping size={50} className="w-16 h-16 text-colorTextPrimary" onClick={toggleModalContactIsOpen} />}
-        
-      </div>
-      {
-        modalContactIsOpen && <ModalContact />
-      }
-      <div className="pt-36">
-        <Outlet />
+      <Indtroduction />
+      <div className="relative bg-gradient-background">
+        <div className="bg-colorContrast right-2 bottom-2 rounded-full flex items-center justify-center p-1 fixed z-50">
+          {modalContactIsOpen ? (
+            <IoCloseOutline
+              size={50}
+              className="w-16 h-16 text-colorTextPrimary"
+              onClick={toggleModalContactIsOpen}
+            />
+          ) : (
+            <TiMessageTyping
+              size={50}
+              className="w-16 h-16 text-colorTextPrimary"
+              onClick={toggleModalContactIsOpen}
+            />
+          )}
+        </div>
+        {modalContactIsOpen && <ModalContact />}
+        <div className="pt-36">
+          <Outlet />
+        </div>
       </div>
       <Footer />
     </main>
-  )
-}
+  );
+};
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,10 @@
 import { AboutMe } from "../../components/AboutMe";
 import { Contact } from "../../components/Contact";
-import { Indtroduction } from "../../components/Logo/Introduction";
 import { Projects } from "../../components/Projects";
 
 export const HomePage = () => {
-
   return (
     <div className="flex flex-col items-center justify-start min-h-screen gap-12">
-      <Indtroduction />
       <div className="flex flex-col w-full px-5">
         <Projects />
       </div>
@@ -17,5 +14,4 @@ export const HomePage = () => {
       </div>
     </div>
   );
-
 };
