c723d229d002c6bded976cf70b3f757f5fce6c66
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-10 11:42:36 -0300

    Project animation

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,36 +1,46 @@
 import { useState } from "react";
 import { ProjectBanner } from "../ProjectBanner";
 import { useNavigate } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 type ProjectProps = {
-  name: string,
-  slug: string,
-  imgBanner: string,
-  primary: boolean
-}
+  name: string;
+  slug: string;
+  imgBanner: string;
+  primary: boolean;
+};
 
-export const Project = ({ imgBanner, name, primary, slug }:ProjectProps) => {
+export const Project = ({ imgBanner, name, primary, slug }: ProjectProps) => {
   const [isVisible, setIsVisible] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <div
+    <motion.div
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
       className="relative flex items-center justify-center"
       onClick={() => navigate(`/project/${slug}`)}
     >
-      <div className={`bg-colorBackgroundModal absolute top-0 right-0 bottom-0 left-0 items-center justify-center rounded-lg cursor-pointer
-        ${isVisible ? "flex" : "hidden"}`}
-      >
-        <h4>{name}</h4>
-      </div>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            transition={{ duration: 0.1 }}
+            className={`bg-colorBackgroundModal absolute top-0 right-0 bottom-0 left-0 items-center justify-center rounded-lg cursor-pointer
+                ${isVisible ? "flex" : "hidden"}`}
+          >
+            <h4>{name}</h4>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <ProjectBanner
         nameProject={name}
         slugProject={slug}
         srcImg={imgBanner}
         primary={primary}
       />
-    </div>
+    </motion.div>
   );
 };
